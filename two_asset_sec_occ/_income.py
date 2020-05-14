5d0e1b98c2a3f223839e46781e59c43b3627bd67
NickShabalin <nick@nshabalin.com>
2020-05-14 02:30:25 +0200

    Support changes from new version

--- two_asset_sec_occ/_income.py
+++ two_asset_sec_occ/_income.py
@@ -13,8 +13,12 @@ def income_labour_supply(w_occ: List[float], gamma_hh: List[float], m: float, N:
 
 
 # noinspection PyPep8Naming
-def income_grid(e_grid: ndarray, tax: float, w: List[float], gamma: List[float], m: float, N: float) -> ndarray:
-    N_occ = [N * m * gamma[i] for i in range(3)]
-    choices = [N_occ[i] * w[i] for i in range(3)]
-    z_grid = (1 - tax) * e_grid * max(choices)
+def income_grid(e_grid: ndarray,
+                tax: float,
+                w_occ: List[float],
+                gamma_hh: List[float],
+                m: float,
+                N_hh_occ: List[float]) -> ndarray:
+    occupation = N_hh_occ.index(max(N_hh_occ))
+    z_grid = (1 - tax) * e_grid * m * gamma_hh[occupation] * w_occ[occupation] * N_hh_occ[occupation]
     return z_grid
